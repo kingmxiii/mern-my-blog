7a79795e0f724d8e5e4907ac0aa221c28bf96612
Rey Polanco <reypolanco@gmail.com>
2020-02-19 21:13:35 -0500

    first commit

---
+++ src/build/precache-manifest.e062334e1c248be7185baeeed486ccb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d1fea87497bd516a5912dfb98e46c9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70d779c2a5c5b0416766",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "3cb37e72c89f550b30a5",
+    "url": "/static/js/2.d508b2ea.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.d508b2ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70d779c2a5c5b0416766",
+    "url": "/static/js/main.e5538797.chunk.js"
+  },
+  {
+    "revision": "1a6842cff40cdbd7ee88",
+    "url": "/static/js/runtime-main.d7f357ff.js"
+  }
+]);
